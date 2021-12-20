63f42d4a741969bed63c3bad5d99413508d9974c
Filip Porębiński <filip.porebinski@o2.pl>
2021-12-20 15:44:37 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+print("zadanie 2")
+new_tab=[]
+for i in range(101):
+    if i%5==0:
+        new_tab.append(i)
+    else:
+        pass
+print(new_tab)
+new_tab1=[number**3 for number in new_tab ]
+print(new_tab1)
